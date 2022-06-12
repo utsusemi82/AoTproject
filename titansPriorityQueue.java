6b58f449e6216a9d318c4d741af6616e69864a29
Yap-Yoong-Siew <93151500+Yap-Yoong-Siew@users.noreply.github.com>
2022-06-12 09:30:56 +0800

    added some comments

--- titansPriorityQueue.java
+++ titansPriorityQueue.java
@@ -9,7 +9,8 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 /**
- *
+ * The design of this priorityQueue is such that all elements are added to a linkedlist
+ * then, when dequeue is called, a for loop is used to search for the titan with the highest risk
  * @author Yap Yoong Siew
  */
 public class titansPriorityQueue {
@@ -27,15 +28,15 @@ public class titansPriorityQueue {
     }
     
     public Titans dequeue(){
-        int risk = oriqueue.get(0).getRisk();
-        int riskIndex = 0;
+        int risk = oriqueue.get(0).getRisk();//get the risk of the first element/titan in the list
+        int riskIndex = 0; //keep track of the indexing
         for(int i = 0; i < oriqueue.size(); i++){//separate the 3 data types
-            if(risk < oriqueue.get(i).getRisk()){
-                risk = oriqueue.get(i).getRisk();
-                riskIndex = i;
+            if(risk < oriqueue.get(i).getRisk()){//if the ith titan's risk is > current risk value
+                risk = oriqueue.get(i).getRisk();//current risk value = the ith titan's risk value
+                riskIndex = i;//the riskIndex will now point to the ith index
             }
         }
-        return oriqueue.remove(riskIndex);
+        return oriqueue.remove(riskIndex);//then remove the highest risk titan from the linkedlist
     }
     
     public Titans getElement(int i){
